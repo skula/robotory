78bc3b76e9050acc4925ea242d36f6a385c16ffe
Stephane <Stephane@192.168.1.22>
2015-07-13 14:04:57 +0200

    hi

---
+++ Robotory/src/com/skula/robotory/constants/UIArea.java
@@ -0,0 +1,54 @@
+package com.skula.robotory.constants;
+
+public class UIArea {
+	// tiles 0 a 23
+	public static final int AREA_TILE_1 = 0;
+	public static final int AREA_TILE_2 = 1;
+	public static final int AREA_TILE_3 = 2;
+	public static final int AREA_TILE_4 = 3;
+	public static final int AREA_TILE_5 = 4;
+	public static final int AREA_TILE_6 = 5;
+	public static final int AREA_TILE_7 = 6;
+	public static final int AREA_TILE_8 = 7;
+	public static final int AREA_TILE_9 = 8;
+	public static final int AREA_TILE_10 = 9;
+	public static final int AREA_TILE_11 = 10;
+	public static final int AREA_TILE_12 = 11;
+	public static final int AREA_TILE_13 = 12;
+	public static final int AREA_TILE_14 = 13;
+	public static final int AREA_TILE_15 = 14;
+	public static final int AREA_TILE_16 = 15;
+	public static final int AREA_TILE_17 = 16;
+	public static final int AREA_TILE_18 = 17;
+	public static final int AREA_TILE_19 = 18;
+	public static final int AREA_TILE_20 = 19;
+	public static final int AREA_TILE_21 = 20;
+	public static final int AREA_TILE_22 = 21;
+	public static final int AREA_TILE_23 = 22;
+	public static final int AREA_TILE_24 = 23;
+
+	// stock joueur blanc
+	public static final int AREA_PLAYER1_STOCK_1 = 100;
+	public static final int AREA_PLAYER1_STOCK_2 = 101;
+	public static final int AREA_PLAYER1_STOCK_3 = 102;
+	public static final int AREA_PLAYER1_STOCK_4 = 103;
+	
+	// stock joueur gris
+	public static final int AREA_PLAYER2_STOCK_1 = 104;
+	public static final int AREA_PLAYER2_STOCK_2 = 105;
+	public static final int AREA_PLAYER2_STOCK_3 = 106;
+	public static final int AREA_PLAYER2_STOCK_4 = 107;
+	
+	// boutons 
+	public static final int AREA_BUTTON_OK = 300;
+	public static final int AREA_BUTTON_STOCK_WHITE = 301;
+	public static final int AREA_BUTTON_STOCK_BLACK = 302;
+
+	// néant
+	public static final int AREA_NONE = -1;
+	
+	public static int getArea(int x, int y){
+		
+		return 0;
+	}
+}
