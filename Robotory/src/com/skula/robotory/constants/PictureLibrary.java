6b91b174305dd8857a6fa0cf493a88cc27de436f
Stephane <Stephane@192.168.1.22>
2015-07-27 19:15:54 +0200

    hi

--- Robotory/src/com/skula/robotory/constants/PictureLibrary.java
+++ Robotory/src/com/skula/robotory/constants/PictureLibrary.java
@@ -2,16 +2,16 @@ package com.skula.robotory.constants;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
 import com.skula.robotory.R;
 
 public class PictureLibrary{
 	private Map<Integer, Bitmap> map;
 	
+	@SuppressLint("UseSparseArrays")
 	public PictureLibrary(Resources res){
 		this.map = new HashMap<Integer, Bitmap>();		
 		this.map.put(R.drawable.background, 		BitmapFactory.decodeResource(res, R.drawable.background));
@@ -22,6 +22,64 @@ public class PictureLibrary{
 
 		this.map.put(R.drawable.spawn_black, 		BitmapFactory.decodeResource(res, R.drawable.spawn_black));
 		this.map.put(R.drawable.spawn_white, 		BitmapFactory.decodeResource(res, R.drawable.spawn_white));
+		
+		this.map.put(R.drawable.btn_bstock_p1_01,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_01));
+		this.map.put(R.drawable.btn_wstock_p1_01,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_01));
+		this.map.put(R.drawable.btn_bstock_p1_02,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_02));
+		this.map.put(R.drawable.btn_wstock_p1_02,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_02));
+		this.map.put(R.drawable.btn_bstock_p1_03,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_03));
+		this.map.put(R.drawable.btn_wstock_p1_03,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_03));
+		this.map.put(R.drawable.btn_bstock_p1_04,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_04));
+		this.map.put(R.drawable.btn_wstock_p1_04,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_04));
+		this.map.put(R.drawable.btn_bstock_p1_05,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_05));
+		this.map.put(R.drawable.btn_wstock_p1_05,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_05));
+		this.map.put(R.drawable.btn_bstock_p1_06,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_06));
+		this.map.put(R.drawable.btn_wstock_p1_06,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_06));
+		this.map.put(R.drawable.btn_bstock_p1_07,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_07));
+		this.map.put(R.drawable.btn_wstock_p1_07,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_07));
+		this.map.put(R.drawable.btn_bstock_p1_08,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_08));
+		this.map.put(R.drawable.btn_wstock_p1_08,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_08));
+		this.map.put(R.drawable.btn_bstock_p1_09,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_09));
+		this.map.put(R.drawable.btn_wstock_p1_09,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_09));
+		this.map.put(R.drawable.btn_bstock_p1_10,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_10));
+		this.map.put(R.drawable.btn_wstock_p1_10,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_10));
+		this.map.put(R.drawable.btn_bstock_p1_11,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_11));
+		this.map.put(R.drawable.btn_wstock_p1_11,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_11));
+		this.map.put(R.drawable.btn_bstock_p1_12,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_12));
+		this.map.put(R.drawable.btn_wstock_p1_12,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_12));
+		this.map.put(R.drawable.btn_bstock_p1_13,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_13));
+		this.map.put(R.drawable.btn_wstock_p1_13,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_13));
+		this.map.put(R.drawable.btn_bstock_p1_14,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p1_14));
+		this.map.put(R.drawable.btn_wstock_p1_14,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p1_14));
+		this.map.put(R.drawable.btn_bstock_p1_01,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_01));
+		this.map.put(R.drawable.btn_wstock_p1_01,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_01));
+		this.map.put(R.drawable.btn_bstock_p1_02,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_02));
+		this.map.put(R.drawable.btn_wstock_p1_02,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_02));
+		this.map.put(R.drawable.btn_bstock_p1_03,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_03));
+		this.map.put(R.drawable.btn_wstock_p1_03,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_03));
+		this.map.put(R.drawable.btn_bstock_p1_04,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_04));
+		this.map.put(R.drawable.btn_wstock_p1_04,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_04));
+		this.map.put(R.drawable.btn_bstock_p1_05,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_05));
+		this.map.put(R.drawable.btn_wstock_p1_05,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_05));
+		this.map.put(R.drawable.btn_bstock_p1_06,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_06));
+		this.map.put(R.drawable.btn_wstock_p1_06,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_06));
+		this.map.put(R.drawable.btn_bstock_p1_07,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_07));
+		this.map.put(R.drawable.btn_wstock_p1_07,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_07));
+		this.map.put(R.drawable.btn_bstock_p1_08,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_08));
+		this.map.put(R.drawable.btn_wstock_p1_08,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_08));
+		this.map.put(R.drawable.btn_bstock_p1_09,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_09));
+		this.map.put(R.drawable.btn_wstock_p1_09,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_09));
+		this.map.put(R.drawable.btn_bstock_p1_10,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_10));
+		this.map.put(R.drawable.btn_wstock_p1_10,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_10));
+		this.map.put(R.drawable.btn_bstock_p1_11,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_11));
+		this.map.put(R.drawable.btn_wstock_p1_11,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_11));
+		this.map.put(R.drawable.btn_bstock_p1_12,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_12));
+		this.map.put(R.drawable.btn_wstock_p1_12,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_12));
+		this.map.put(R.drawable.btn_bstock_p1_13,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_13));
+		this.map.put(R.drawable.btn_wstock_p1_13,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_13));
+		this.map.put(R.drawable.btn_bstock_p1_14,		BitmapFactory.decodeResource(res, R.drawable.btn_bstock_p2_14));
+		this.map.put(R.drawable.btn_wstock_p1_14,		BitmapFactory.decodeResource(res, R.drawable.btn_wstock_p2_14));
+
 	}
 		
 	public Bitmap get(int id){
